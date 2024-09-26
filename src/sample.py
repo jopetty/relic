a40e4e08ca4020e004c185bc09e037620bee0829
jopetty <research@jacksonpetty.org>
2024-09-26 16:01:41 -0400

    demo some sampling from grammars

---
+++ src/sample.py
@@ -0,0 +1,36 @@
+import logging
+import pyrootutils
+import fire
+from utils.utils import get_logger
+from pathlib import Path
+
+import grammar
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%d-%m %H:%M:%S",
+    level=logging.INFO,
+)
+
+log = get_logger(__name__)
+
+PROJECT_ROOT = path = pyrootutils.find_root(
+    search_from=__file__, indicator=".project-root"
+)
+
+
+def sample_expression(
+    grammar_file: Path | str = Path("data/untyped_lambda_calculus.cfg"),
+    n_samples: int = 5,
+):
+    if isinstance(grammar_file, str):
+        grammar_file = Path(grammar_file)
+
+    g = grammar.Grammar.from_grammar(grammar_file)
+    generations = list(g.generate(n_samples=n_samples))
+    for i, gen in enumerate(generations):
+        print(f"{i+1}: {gen}")
+
+
+if __name__ == "__main__":
+    fire.Fire(sample_expression)
